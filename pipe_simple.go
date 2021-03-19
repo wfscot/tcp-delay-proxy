ad60b8bdcdb7964b0c89c580a8e2463e32fbac7f
Scot Hastings <6472550+wfscot@users.noreply.github.com>
2021-03-19 13:40:52 -0600

    Update simple pipe to use a context (currently TODO) to allow cancellation from parent

--- pipe_simple.go
+++ pipe_simple.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/rs/zerolog"
 	"io"
 	"net"
@@ -17,50 +18,88 @@ func newSimplePipe(src net.Conn, dst net.Conn) pipe {
 }
 
 func (p *simplePipe) Run(log zerolog.Logger) {
+	// TODO - get from parent
+	ctx := context.TODO()
+
+	// use the log object from the context
+	//log := log.Ctx(ctx).With().Str("func", "simplePipe.Run").Logger()
+	log = log.With().Str("func", "simplePipe.Run").Logger()
+
+	// use a static buffer of 1MB
 	bbuf := make([]byte, 1024*1024)
 
-	// disable deadlines
-	err := p.src.SetReadDeadline(time.Time{})
+	// disable deadlines for now. note the loop below will set the source read deadline.
+	err := p.src.SetDeadline(time.Time{})
 	if err != nil {
-		log.Fatal().Err(err).Msg("error while setting read deadline")
+		log.Error().Err(err).Msg("error while setting read deadline")
+		return
 	}
-	err = p.dst.SetWriteDeadline(time.Time{})
+	err = p.dst.SetDeadline(time.Time{})
 	if err != nil {
-		log.Fatal().Err(err).Msg("error while setting write deadline")
+		log.Error().Err(err).Msg("error while setting write deadline")
+		return
 	}
-	// just copy bytes in an infinite loop
+
+	// receive bytes in an infinite loop
 	for {
-		nb, err := p.src.Read(bbuf)
-		if err == io.EOF {
-			// this is a normal close. exit the loop.
-			log.Info().Msg("connection closed by source")
-			break
-		} else if err != nil {
-			log.Fatal().Err(err).Msg("error while reading from connection")
-		}
-		log.Info().Int("numBytes", nb).Msg("read bytes")
-		// this really should go through in one write call, but just in case, allow for partial writes and keep a write cursor
-		wc := 0
-		for wc < nb {
-			n, err := p.dst.Write(bbuf[wc:nb])
-			if err == io.EOF {
-				// this is a normal close. exit the loop.
-				log.Info().Msg("connection closed by dest")
-				break
+		// use a select to allow for cancelling via context
+		select {
+		case <-ctx.Done():
+			// if the context has been cancelled, just return
+			log.Debug().Msg("exiting due to cancelled context")
+			return
+
+		default:
+			// otherwise, set a read deadline a short time in the future and attempt to read. this allows us to periodically
+			// check for context cancellation
+			err := p.src.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+			if err != nil {
+				log.Error().Err(err).Msg("error while setting source read deadline")
+				return
+			}
+			nb, err := p.src.Read(bbuf)
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				// this is a normal read timeout due to deadline. nothing to see here.
+				log.Trace().Msg("read timeout. continuing...")
+				continue
+			} else if err == io.EOF {
+				// this is a normal close. return nil.
+				log.Info().Msg("connection closed by source")
+				return
 			} else if err != nil {
-				log.Fatal().Err(err).Msg("error while writing to connection")
+				// for any other error, return it. this should result in the context getting torn down.
+				log.Error().Err(err).Msg("error while reading from connection")
+				return
 			}
+			// otherwise we have some data. write it immediately
+			log.Info().Int("numBytes", nb).Msg("read bytes")
 
-			// shouldn't happen, but just in case
-			if n < 0 {
-				log.Fatal().Msg("wrote negative bytes")
-			} else if n == 0 {
-				log.Fatal().Msg("wrote zero bytes")
+			// this really should go through in one write call, but just in case, allow for partial writes and keep a write cursor
+			wc := 0
+			for wc < nb {
+				n, err := p.dst.Write(bbuf[wc:nb])
+				if err == io.EOF {
+					// this is a normal close. exit the loop.
+					log.Info().Msg("connection closed by dest")
+					break
+				} else if err != nil {
+					log.Error().Err(err).Msg("error while writing to connection")
+					return
+				}
+
+				// shouldn't happen, but just in case
+				if n < 0 {
+					log.Error().Msg("wrote negative bytes. aborting.")
+					return
+				} else if n == 0 {
+					log.Error().Msg("wrote zero bytes. aborting.")
+					return
+				}
+
+				// otherwise we wrote some bytes. increment the counter
+				log.Debug().Int("numBytes", nb).Msg("wrote bytes")
+				wc += n
 			}
-
-			// otherwise we wrote some bytes. increment the counter
-			log.Debug().Int("numBytes", nb).Msg("wrote bytes")
-			wc += n
 		}
 	}
 }
