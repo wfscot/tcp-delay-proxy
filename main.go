c9f38aa8f9506476dbb984afb54cda4b9e130fdb
Scot Hastings <6472550+wfscot@users.noreply.github.com>
2021-03-19 15:34:27 -0600

    Move connection functionality to dedicated connection object

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"sync"
 	"time"
 )
 
 func main() {
 	// configure the zerolog for pretty commmand line feedback
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	// add fields to logger
+	log := log.With().Str("func", "main").Logger()
 
 	// default log level is warn
 	// TODO - adjust to warn
@@ -56,65 +58,17 @@ func main() {
 }
 
 func handleConnection(log zerolog.Logger, clientConn net.Conn) {
-	defer clientConn.Close()
-	log.Debug().Msg("handling connection")
+	log = log.With().Str("func", "main.handleConnections").Logger()
 
-	// establish upstream connection
-	log.Debug().Msg("establishing upstream connection")
-	upstreamConn, err := net.Dial("tcp", "localhost:9401")
+	// set up connection
+	conn := proxy.NewDelayedConnection(1*time.Second, 0, "localhost:9401")
+
+	// put logger in context
+	ctx := log.WithContext(context.TODO())
+
+	// run connection
+	err := conn.Run(ctx, clientConn)
 	if err != nil {
-		log.Error().Err(err).Msg("error establishing upstream connection")
-		return
+		log.Error().Err(err).Msg("connection exited with error")
 	}
-	log.Info().Stringer("upstreamAddr", upstreamConn.RemoteAddr()).Msg("upstream connection established")
-
-	// set up pipes for handling traffic in both directions
-	upPipe := proxy.NewDelayedPipe(clientConn, upstreamConn, 1*time.Second)
-	downPipe := proxy.NewSimplePipe(upstreamConn, clientConn)
-	log.Debug().Msg("pipes established")
-
-	ctx := context.TODO()
-
-	// run the up and down pipes separately
-	// use a context both to tear down the children as well as to encapsulate the logger
-	ctx, cancel := context.WithCancel(ctx)
-	ctx = log.WithContext(ctx)
-
-	// run pipes in separate go routines
-	// note that we don't have to explicitly handle context cancellation here as it's handled by the children
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		// enhance logger and store in context
-		log := log.With().Str("direction", "up").Logger()
-		ctx := log.WithContext(ctx)
-		log.Debug().Msg("running up pipe")
-		err := upPipe.Run(ctx)
-		if err != nil {
-			log.Error().Err(err).Msg("up pipe exited with error")
-		}
-		log.Debug().Msg("up pipe finished")
-		cancel()
-		wg.Done()
-	}()
-	wg.Add(1)
-	go func() {
-		// enhance logger and store in context
-		log := log.With().Str("direction", "down").Logger()
-		ctx := log.WithContext(ctx)
-		log.Debug().Msg("running down pipe")
-		err := downPipe.Run(ctx)
-		if err != nil {
-			log.Error().Err(err).Msg("down pipe exited with error")
-		}
-		log.Debug().Msg("down pipe finished")
-		cancel()
-		wg.Done()
-	}()
-	log.Info().Msg("all pipes running")
-
-	// wait for all pipes to complete
-	log.Debug().Msg("waiting for pipes to finish")
-	wg.Wait()
-	log.Info().Msg("all pipes finished. closing connection.")
 }
---
+++ proxy/connection.go
@@ -0,0 +1,120 @@
+package proxy
+
+import (
+	"context"
+	"github.com/rs/zerolog/log"
+	"net"
+	"sync"
+	"time"
+)
+
+// defines a generic connection object
+// a connection represents a single proxy server session created in response to a single client connection
+// for now, we just have a single implementation of connection so it's contained in this file.
+
+type Connection interface {
+	Run(ctx context.Context, clientConn net.Conn) error
+}
+
+type connection struct {
+	upDelay      time.Duration
+	downDelay    time.Duration
+	upstreamAddr string
+}
+
+func NewDelayedConnection(upDelay time.Duration, downDelay time.Duration, upStreamAddr string) Connection {
+	return &connection{
+		upDelay:      upDelay,
+		downDelay:    downDelay,
+		upstreamAddr: upStreamAddr,
+	}
+}
+
+func (c *connection) Run(ctx context.Context, clientConn net.Conn) error {
+	// use the log object from the context with additional fields
+	log := log.Ctx(ctx).With().Str("func", "connection.Run").Logger()
+
+	// we own the client connection. make sure it's closed.
+	defer clientConn.Close()
+
+	log.Debug().Msg("handling connection")
+
+	// establish upstream connection
+	log.Debug().Msg("establishing upstream connection")
+	upstreamConn, err := net.Dial("tcp", c.upstreamAddr)
+	if err != nil {
+		log.Error().Err(err).Str("upstreamAddr", c.upstreamAddr).Msg("error establishing upstream connection")
+		return err
+	}
+	log = log.With().Stringer("upstreamAddr", upstreamConn.RemoteAddr()).Logger()
+	log.Info().Msg("upstream connection established")
+	defer upstreamConn.Close()
+
+	// set up pipes for handling traffic in both directions. if delay is zero, use a simple pipe.
+	var upPipe, downPipe Pipe
+	if c.upDelay.Nanoseconds() == 0 {
+		log.Debug().Msg("using simple up pipe")
+		upPipe = NewSimplePipe(clientConn, upstreamConn)
+	} else {
+		log.Debug().Dur("upDelay", c.upDelay).Msg("using delayed up pipe")
+		upPipe = NewDelayedPipe(clientConn, upstreamConn, c.upDelay)
+	}
+	if c.downDelay.Nanoseconds() == 0 {
+		log.Debug().Msg("using simple down pipe")
+		downPipe = NewSimplePipe(upstreamConn, clientConn)
+	} else {
+		log.Debug().Dur("downDelay", c.downDelay).Msg("using delayed down pipe")
+		downPipe = NewDelayedPipe(upstreamConn, clientConn, c.downDelay)
+	}
+	log.Info().Msg("pipes established")
+
+	// run the up and down pipes separately
+	// use a context both to tear down the children as well as to encapsulate the logger
+	ctx, cancel := context.WithCancel(ctx)
+	ctx = log.WithContext(ctx)
+
+	// remember the last error
+	var lastErr error
+
+	// run pipes in separate go routines
+	// note that we don't have to explicitly handle context cancellation here as it's handled by the children
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		// enhance logger and store in context
+		log := log.With().Str("direction", "up").Logger()
+		ctx := log.WithContext(ctx)
+		log.Debug().Msg("running up pipe")
+		err := upPipe.Run(ctx)
+		if err != nil {
+			log.Error().Err(err).Msg("up pipe exited with error")
+			lastErr = err
+		}
+		log.Debug().Msg("up pipe finished")
+		cancel()
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		// enhance logger and store in context
+		log := log.With().Str("direction", "down").Logger()
+		ctx := log.WithContext(ctx)
+		log.Debug().Msg("running down pipe")
+		err := downPipe.Run(ctx)
+		if err != nil {
+			log.Error().Err(err).Msg("down pipe exited with error")
+			lastErr = err
+		}
+		log.Debug().Msg("down pipe finished")
+		cancel()
+		wg.Done()
+	}()
+	log.Info().Msg("all pipes running")
+
+	// wait for all pipes to complete
+	log.Debug().Msg("waiting for pipes to finish")
+	wg.Wait()
+	log.Info().Msg("all pipes finished. closing connection.")
+
+	return lastErr
+}
